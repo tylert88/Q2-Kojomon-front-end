375b51fc2ee62b7b099201bc47565a2653f1ea39
Nate Bauer <n8bdoesemailvia@gmail.com>
2017-09-20 19:18:44 -0600

    Update play.js
    Updated merge confilcts

--- master/Landing-page/play.js
+++ master/Landing-page/play.js
@@ -3,12 +3,9 @@
 //***ID of '1' Must be changed to the id of the
 // active pet of the user who is logged in***
 
-<<<<<<< HEAD
 
 getPetNameByPetId(petId).then(appendPetName)
-=======
-getPetNameByPetId(1).then(appendPetName)
->>>>>>> 6565eb95af17365575508ac34852ad1cad0104fb
+
 
 $('.hidden-name-change').hide()
 
@@ -24,13 +21,9 @@ $('#change-name').click(function() {
 $('#submit-new-name').click(function(){
   let newKojoName = $('#new-name-input').val()
 
-<<<<<<< HEAD
   updatePetNameById(petId,newKojoName)
-  $('.pet-name').replaceWith(`<h2> ${newKojoName} </h2>`)
-=======
-  updatePetNameById(1,newKojoName)
   $('.pet-name').replaceWith(`<h2 class="pet-name"> ${newKojoName} </h2>`)
->>>>>>> 6565eb95af17365575508ac34852ad1cad0104fb
+
 })
 
 $(".selectable").click(function() {
@@ -42,10 +35,7 @@ $(".selectable").click(function() {
 
 $('.center-image').click(function() {
   var selectedAction = $('.selected').data('action')
-  //Logic for game after this. selectedAction will be either
-  // 'feed' 'clean' or 'pet' based on the currently selected action
-<<<<<<< HEAD
-  //Food: update lasteinteraction
+  
 
   if (selectedAction === 'feed') {
     if (foodScore >= 24){
@@ -61,7 +51,4 @@ $('.center-image').click(function() {
   }
 }
 
-
-=======
->>>>>>> 6565eb95af17365575508ac34852ad1cad0104fb
 })
