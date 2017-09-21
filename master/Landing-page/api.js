1d9bf4a12a5f9a0c4f4c79066bc65e9b9215b142
Nate Bauer <n8bdoesemailvia@gmail.com>
2017-09-20 19:13:49 -0600

    Update api.js
    Removed weird HEAD tags

--- master/Landing-page/api.js
+++ master/Landing-page/api.js
@@ -1,17 +1,11 @@
 
 //variable has to be changed to the backend once on  HEROKU
-<<<<<<< HEAD
+
 const herokuAPI = "https://gentle-peak-99253.herokuapp.com/api/v1/"
 
 function getPetNameByPetId(id){
   return fetch(herokuAPI + 'pet/' + id)
-=======
 
-const herokuAPI = "https://gentle-peak-99253.herokuapp.com/api/v1/"
-
-function getPetNameByPetId(id){
-  return fetch(localHost + 'pet/' + id)
->>>>>>> 6565eb95af17365575508ac34852ad1cad0104fb
   .then(res => res.json())
   .then(res => res[0])
   .catch(error => console.log(error))
@@ -55,7 +49,7 @@ function getFoodInteractionTimeById(id) {
 
 function updateEvoScoreById(id, evoData){
 
-<<<<<<< HEAD
+
 var settings = {
   "async": true,
   "crossDomain": true,
@@ -70,7 +64,5 @@ var settings = {
     evo_score: evoData
   })
 }
-=======
 
->>>>>>> 6565eb95af17365575508ac34852ad1cad0104fb
 }
