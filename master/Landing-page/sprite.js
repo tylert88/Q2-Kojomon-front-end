3a99a87119850b835a2eb5f4d29c60becf8557f5
Joscelyn James <josce.james7@hotmail.com>
2017-09-21 19:31:57 -0600

    fixed quotes

--- master/Landing-page/sprite.js
+++ master/Landing-page/sprite.js
@@ -11,11 +11,11 @@ function appendPlantSprite(results){
  const plantType = results.plant_type;
 
   if(plant.evo_score >= 120) {
-    $('.center-image').append(`<img src=${plantType.evo_3}/>`)
+    $('.center-image').append(`<img src='${plantType.evo_3}'/>`)
   } else if (plant.evo_score >= 48) {
-    $('.center-image').append(`<img src=${plantType.evo_2}/>`)
+    $('.center-image').append(`<img src='${plantType.evo_2}'/>`)
   } else {
-    $('.center-image').append(`<img src=${plantType.evo_1}/>`)
+    $('.center-image').append(`<img src='${plantType.evo_1}'/>`)
 
   }
 };
